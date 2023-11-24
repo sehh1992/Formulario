8e68991aba4fd516658b5a46c9c75044a232e9a9
saul hernandez <sehh2023@gmail.com>
2023-11-24 14:43:12 -0600

    /

--- src/app/saludo/page.jsx
+++ src/app/saludo/page.jsx
@@ -1,7 +1,7 @@
 export default function saludo() {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold text-center">Hola a todos</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Hola a todos</h1>
+    </div>
+  );
 }
