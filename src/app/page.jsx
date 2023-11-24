22793a7e1f881850f2bb1c0681fc83ed15bd7da8
saul hernandez <sehh2023@gmail.com>
2023-11-24 11:50:34 -0600

    se agregaron expreciones regulares

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,3 @@
 export default function newPage() {
-    return (
-        <h1 className="text-3xl font-bold text-center">Pagina Inicial</h1>
-    )
+  return <h1 className="text-3xl font-bold text-center">Pagina Inicial</h1>;
 }
